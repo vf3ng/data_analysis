738e6e7cdf9014b12f11df35ba1bd2a14b64d6f9
leon <450760609@qq.com>
2016-02-23 11:40:23 +0800

    .

--- qianhai/blacklist/DesCrypter.py
+++ qianhai/blacklist/DesCrypter.py
@@ -2,19 +2,29 @@
 from M2Crypto.EVP import Cipher
 import base64
 
+key = 'SK803@!QLF-D25WEDA5E52DA'
 
 class DesCrypter(object):
-    def __init__(self, key):
+    def __init__(self):
         self.key = key
         self.iv = 16 * '\x00'
 
-    def encrypt_3des(key, text):
-        encryptor = Cipher(alg='des_ede3_ecb', key=key, op=1, iv='\0'*16)
+    def encrypt_3des(self,text):
+        encryptor = Cipher(alg='des_ede3_ecb', key=self.key, op=1, iv=self.iv)
         s = encryptor.update(text)
-        return s+ encryptor.final()
+        return base64.b64encode(s+ encryptor.final())
 
-    def decrypt_3des(key, text):
-        decryptor = Cipher(alg='des_ede3_ecb', key=key, op=0, iv='\0'*16)
-        s= decryptor.update(text)
+    def decrypt_3des(self,text):
+        data = base64.b64decode(text)
+        decryptor = Cipher(alg='des_ede3_ecb', key=key, op=0, iv=self.iv)
+        s= decryptor.update(data)
         return s + decryptor.final()
+
+if __name__ == "__main__":
+    d = DesCrypter()
+    text = 'value'
+    encrypt_text =d.encrypt_3des(text)
+    print encrypt_text
+    print  d.decrypt_3des(encrypt_text)
+    
 
