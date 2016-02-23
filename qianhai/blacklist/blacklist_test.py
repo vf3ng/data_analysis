36e274eeb3af89b6291cda671ccd7fbeb8507ebb
leon <450760609@qq.com>
2016-02-23 17:40:16 +0800

    .

--- qianhai/blacklist/blacklist_test.py
+++ qianhai/blacklist/blacklist_test.py
@@ -1,20 +1,22 @@
 #coding=utf-8
 import urllib2,json,urllib
-from Crypto.Cipher import AES
 import base64,hashlib
 from DesCrypter import DesCrypter
 #from data_user import *
 import time,datetime
 import requests
 import random
-
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5 as pk
+from Crypto.Hash import SHA
 #db.generate_mapping(create_tables = True)
 
 userName = 'V_PA025_QHCS_DCS'
 userPassword = 'weblogic1'
 key = 'SK803@!QLF-D25WEDA5E52DA'
 
-
+privatekey=RSA.importKey(open('private.pem.key','r').read())
+test_url = 'https://test-qhzx.pingan.com.cn:5443/do/dmz/query/blacklist/v1/MSC8004'
 
 class TempUser(object):
       def __init__(self,id,status,code,name,id_card_num,phone,home_addr,work_addr):
@@ -33,20 +35,13 @@ class BlacklistCal(object):
           self.lines = f.readlines()
           f.close()
           self.user_list = []
-          self.init_dict()
-          self.total = len(self.user_list)
-          self.hit_count = 0
-          self.temp_count = 0
-          self.hit_only = 0
-          self.temp_only = 0
-          self.hit_good = 0
-          self.temp_good = 0
-          self.response = []
-          self.hit_dict = {}
+          self.init_user()
+          random.seed(time.time())
+          self.batch_num = random.randint(10000,99999)
       def get_header(self):
           random.seed(time.time())
           transNo = random.randint(10000,99999)
-          now = datetime..datetime.strftime(datetime.datetime.now(),"%Y-%m-%d %H:%M:%S")
+          now = datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d %H:%M:%S")
           transDate = now
           authDate = now
           header = {
@@ -61,75 +56,79 @@ class BlacklistCal(object):
           header["transDate"] = transDate
           header["authDate"] = authDate
           return header
+
       def get_sign(self,data):
+          temp = []
+          keys=data.keys()
+          keys.sort()
+          for v in keys:
+              temp.append(str(data[v]))
+          str1=''.join(temp)
+          h=SHA.new(str1)
+          signer = pk.new(privatekey)
+          signn=signer.sign(h)
+          signn=base64.b64encode(signn)
+          return signn
           
+      def make_security_info(self,sign):
+          password = hashlib.sha1(userPassword).hexdigest()
+          securityInfo = {
+              "signatureValue":sign,
+              "userName":userName,
+              "userPassword":password,
+          }
+          return securityInfo
       #need to overwrite
       def make_data(self,user):  
           data = {
+              "batchNo":self.batch_num,
               "idNo":user.id_card_num,
               "idType":'0',
               "reasonNo":'01',
               "name":user.name,
-              "entityAuthCode":,
+              "entityAuthCode":user.id,
+              "entityAuthDate":datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d %H:%M:%S"),
+              "seqNo":user.id
           }
-
+          return data
               
-      def init_dict(self):
+      def init_user(self):
           for i in self.lines:
-              id,check_status,code,name,id_card_num,phone,home_addr,work_addr = i.split()
-              self.user_list.append(TempUser(id,check_status,code,name,id_card_num,phone,home_addr,work_addr))
+              s = len(i.split())
+              if s == 6:
+                  id,check_status,code,name,id_card_num,phone = i.split()
+                  self.user_list.append(TempUser(str(id),str(check_status),str(code),str(name),str(id_card_num),str(phone),'',''))
+              else:
+                  list1 = i.split()[0:8]
+                  id,check_status,code,name,id_card_num,phone,home_addr,work_addr = list1
+                  self.user_list.append(TempUser(str(id),str(check_status),str(code),str(name),str(id_card_num),str(phone),str(home_addr),str(work_addr)))
 
-      def count(self,res,key):
-          try:
-              json_data = json.loads(res)
-              if json_data["status"]==200 and json_data["content"]:
-                  self.temp_count += 1
-                  if self.user_dict[key][0] == '3':
-                      self.temp_good += 1
-                  if self.user_dict[key][1] not in [str(-i) for i in range(2,11)]:
-                      self.temp_only += 1
-
-                  if not self.hit_dict.get(key):
-                      self.hit_dict.setdefault(key,res)
-                      self.hit_count+=1
-                      if self.user_dict[key][0] == '3':
-                          self.hit_good += 1
-                      if self.user_dict[key][1] not in [str(-i) for i in range(2,11)]:
-                          self.hit_only += 1
-              
-          except Exception,e:
-              print e
-
-      def get_response(self,url,data):
+      def get_response(self,url):
           a = open("result","w")
-          for key,val in self.user_dict.items():
-                  user = DataUser.get(user_id=key)
-                  data = {"app_id":app_key,"secret":secret,"idNumber":user.id_card_num.encode("utf-8"),"phone":user.phone_num.encode("utf-8"),"name":user.name.encode("utf-8")}
-                  s = sorted(data.items(), key=lambda d:d[0])
-                  str1=''
-                  for i in s:
-                      str1+= i[0]+'='+i[1]
-                  str1 += md5_key
-                  m = hashlib.md5()
-                  m.update(str1)
-                  token = m.hexdigest()
-                  data.setdefault("token",token)
-                  str_data = json.dumps(data)
-                  aes = AesCrypter(secret)
-                  b64 = aes.encrypt(str_data)
-                  res = json_http_post(url, b64)
-                  s = aes.decrypt(res)
-                  self.count(s,key)
-                  self.response.append(s)
-                  a.write(key+' '+s+' '+ url+'\n')
-                  time.sleep(0.1)
-          print self.temp_good,self.temp_only,self.temp_count,url
-          print self.total,self.hit_good,self.hit_only,self.hit_count
+          for user in self.user_list:
+              header = self.get_header()
+              data = self.make_data(user)
+              sign = self.get_sign(data)
+              securityInfo = self.make_security_info(sign)
+              d = DesCrypter()
+              encrypt_data = d.encrypt_3des(json.dumps(data,ensure_ascii=False))
+              json_data = {
+                  "header":header,
+                  "busiData":encrypt_data,
+                  "securityInfo":securityInfo,
+              }
+              str_data = json.dumps(json_data,ensure_ascii=False)
+              print str_data
+              http_headers = {'content-type': 'application/json;charset=utf-8','content-length':len(str_data)}
+              res = requests.post(url, data=str_data, headers=http_headers ,cert='credoo_ssl.crt',verify=True)
+              res = d.decrypt_3des(res)
+              a.write(user.id+' '+res+'\n')
+              time.sleep(0.1)
+              break
           a.close()
-          return self.hit_dict
 
-b = BlacklistCal("b3")
-print b.get_response()
+b = BlacklistCal("user_data")
+print b.get_response(test_url)
 '''
 if __name__ == '__main__':
     data = {"app_id":app_key,"secret":secret,"idNumber":"511181199306050015","phone":"15608071193","name":"leon"}
