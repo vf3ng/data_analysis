7b9b6699c7ca659e417aa72d1b8982788f260650
leon <450760609@qq.com>
2016-03-04 16:40:53 +0800

    .

--- qianhai/blacklist/credoo_test.py
+++ qianhai/blacklist/credoo_test.py
@@ -30,6 +30,35 @@ class CredooCal(BlacklistCal):
               ]
           }
           return data
+
+
+      def make_data_many(self,user_list):
+          data = {
+              "batchNo":self.batch_num,
+              "records":[]
+          }
+          for user in user_list:
+              new_data ={
+                  "idNo":user.id_card_num,
+                  "idType":'0',
+                  "reasonNo":'01',
+                  "name":user.name,
+                  "entityAuthCode":user.id,
+                  "entityAuthDate":datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d %H:%M:%S"),
+                  "seqNo":user.id,
+                  "mobileNo":'1358629551',
+                  "cardNo":'111111222222',
+                  "email":'421833098@qq.com',
+                  "weiboNo":'421222833@qq.com',
+                  "qqNo":'421833098',
+                  "weixinNo":'412933098',
+                  "taobaoNo":'123123123',
+                  "jdNo":'42183330',
+                  "amazonNo":'asdasd',
+                  "yhdNo":'asdasd'
+              }
+              data['records'].append(new_data)
+          return data
               
 
 if __name__ == '__main__':
